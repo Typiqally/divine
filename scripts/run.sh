620c6bd85fdf6db283f39ec060f8ebe54f56def5
Typiqally <jellemaas321@gmail.com>
2019-02-06 22:06:25 +0100

    Fix run.sh paths

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 printf '\033[8;35;100t'
+cd C:/Users/Typically/Documents/Projects/Java/Divine
 ./gradlew build
-java -jar ../build/libs/divine-1.0-SNAPSHOT.jar -d C:/Users/Typically/.divine/gamepack.jar C:/Users/Typically/.divine/gamepack_out.jar
+java -jar ./build/libs/divine-1.0-SNAPSHOT.jar -d C:/Users/Typically/.divine/gamepack.jar C:/Users/Typically/.divine/gamepack_out.jar
 printf "\033[0m"
 ./gradlew clean
